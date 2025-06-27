cb64917d3e3cdca30127180639b54102a0970048
Nel-tech <favournelson82@gmail.com>
2025-06-27 23:55:43 +0100

    Remove unused erapper prop from SignalProps interface in OneSignalWrapper component for cleaner code.

--- snap/client/src/components/OneSignalWrapper.tsx
+++ snap/client/src/components/OneSignalWrapper.tsx
@@ -4,7 +4,7 @@ import { useEffect, ReactNode } from "react";
 
 interface SignalProps {
     children: ReactNode;
-    erapper:string;
+   
 }
 
 const OneSignalWrapper = ({ children }: SignalProps) => {
