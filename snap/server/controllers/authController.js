623c1becd38fc5135df291d96fa8e7f87cd57014
Nel-tech <favournelson82@gmail.com>
2025-06-30 10:58:18 +0100

    Refactor cookie options in createSendToken function of authController to enhance security; ensure secure flag is set based on production environment and improve code readability.

--- snap/server/controllers/authController.js
+++ snap/server/controllers/authController.js
@@ -15,16 +15,14 @@ const signToken = (id) =>
 
 const createSendToken = (user, statusCode, req, res) => {
   const token = signToken(user._id);
-  
-  const cookieOptions = {
-    expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
-    ),
-    httpOnly: true,
-    secure: req.secure || req.headers['x-forwarded-proto'] === 'https', 
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax', 
-  };
-  
+const cookieOptions = {
+  expires: new Date(
+    Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+  ),
+  httpOnly: true,
+  secure: process.env.NODE_ENV === 'production', 
+  sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+};
   res.cookie('jwt', token, cookieOptions);
   
   user.password = undefined;
